abb5254374e625554abdda79177b9ce121a23c58
Diwakar Vishwakarma <65496452+diwvish1407@users.noreply.github.com>
2021-07-11 18:48:21 +0530

    Create productoftwonum.cpp

---
+++ productoftwonum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x, y;
+
+    cout << "Enter the first number : " << endl;
+    cin >> x;
+    cout << "Enter the second number : " << endl;
+    cin >> y;
+
+    cout << "Product of the two entered numbers is : " << x * y << endl;
+}
